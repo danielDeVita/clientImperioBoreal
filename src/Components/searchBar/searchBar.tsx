d7c6c8590ad3f7c0bf580fffffcf4faced6c3656
danielDeVita <danielitodevita@gmail.com>
2023-03-16 16:22:39 -0300

    added axios functionality

--- src/Components/searchBar/searchBar.tsx
+++ src/Components/searchBar/searchBar.tsx
@@ -1,4 +1,5 @@
 import React, { useState, ChangeEvent, FormEvent } from 'react';
+import axios from 'axios';
 
 const searchBar: React.FC = () => {
     const [query, setQuery] = useState<string>("");
@@ -7,16 +8,21 @@ const searchBar: React.FC = () => {
         setQuery(e.target.value)
     };
 
-    const handleSearch = (e: FormEvent<HTMLFormElement>) => {
+    const handleSearch = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         // Do something with the query here, like search for it
+        let response = await axios.get(`http://localhost:3001/products?query=${query}`)
+        let product = response.data
     };
 
     return (
-        <form onSubmit={handleSearch}>
-            <input type="search" placeholder='Buscar...' onChange={handleChange} value={query} />
-            <button type="submit">Buscar</button>
-        </form>
+        <>
+            <form onSubmit={handleSearch}>
+                <input type="search" placeholder='Buscar...' onChange={handleChange} value={query} />
+                <button type="submit">Buscar</button>
+            </form>
+        </>
+
     );
 };
 
