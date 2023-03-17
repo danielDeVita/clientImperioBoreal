6cb3bc02304e2f1560f1ce9125338d340f081cb8
danielDeVita <danielitodevita@gmail.com>
2023-03-17 10:46:42 -0300

    cleanUp de App, ahora esta limpia

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { useState } from "react";
-import CreateProductForm from "./Components/CreateProductForm/CreateProductForm";
-import SearchBar from "./Components/SearchBar/SearchBar";
+
 //Si tienen problemas con el import en minuscula o mayuscula 
 //es por el cache de vscode, tienen que cerrar y volver a abrirlo
 
 function App() {
   return (
     <div className='App'>
-      <SearchBar />
-      <CreateProductForm />
+    
     </div>
   );
 }
