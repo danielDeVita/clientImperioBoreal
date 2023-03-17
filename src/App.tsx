b35349a55bda00757fd7ea5244521cf683a9c1e8
Victoria Wagner Trento <victoriaawagnertrento@gmail.com>
2023-03-17 22:42:13 +0100

    Probando el form update

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Home from './Components/Home/Home';
 import Dashboard from './Components/Dashboard/Dashboard';
 import { Routes, Route } from "react-router";
 import CreateProductForm from "./Components/CreateProductForm/CreateProductForm";
+import UpdateProductForm from './Components/UpdateProductForm/UpdateProductForm';
 
 //Si tienen problemas con el import en minuscula o mayuscula 
 //es por el cache de vscode, tienen que cerrar y volver a abrirlo
@@ -12,6 +13,7 @@ function App() {
       <Route path="/" element={<Home/>}/>
       <Route path="/dashboard" element={<Dashboard/>}/>
       <Route path="/productform" element={<CreateProductForm/>}/>
+      <Route path="/updateform" element={<UpdateProductForm/>}/>
     </Routes>
   );
 }
---
+++ src/Components/UpdateProductForm/UpdateProductForm.tsx
@@ -0,0 +1,107 @@
+import React, { useState } from "react";
+import axios from 'axios';
+import style from './UpdateProductForm.module.css'
+
+
+interface Product {
+  descriptionName: string;
+  category: string;
+  price: number;
+  priceBusiness: number;
+  priceVAT: number;
+  priceVATBusiness: number;
+}
+
+interface Errors {
+  descriptionName: string;
+  category: string;
+  price: string;
+  priceBusiness: string;
+  priceVAT: string;
+  priceVATBusiness: string;
+}
+
+const validate = (product: Product) => {
+  // Validation logic here
+  return {};
+};
+
+const UpdateProductForm: React.FC = () => {
+  const [product, setProduct] = useState<Product>({
+    descriptionName: "",
+    category: "",
+    price: 0,
+    priceBusiness: 0,
+    priceVAT: 0,
+    priceVATBusiness: 0,
+  });
+
+  const [errors, setErrors] = useState<Errors>({
+    descriptionName: "",
+    category: "",
+    price: "",
+    priceBusiness: "",
+    priceVAT: "",
+    priceVATBusiness: "",
+  });
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setProduct({
+      ...product,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    try {
+    // Sin la fn Validate no anda el form!!! 
+    // if (Object.keys(errors).length === 0) {
+        await axios.put(`http://localhost:3001/products/${id}`, product);
+        console.log(product)
+        alert("Producto modificado");
+    //     } else {
+    //     alert("Faltan completar campos")
+    //   }
+    } catch (error) {
+      console.error(error)
+    }
+  };
+
+    return (
+        <form className={style.formContainer} onSubmit={handleSubmit}>
+
+
+            <h1 className={style.formTitulo}>Modifique su producto</h1>
+
+
+            <label className={style.formLabel} htmlFor="descriptionName">Nombre: </label>
+            <input className={style.formInput} value={product.descriptionName} onChange={handleInputChange} id='descriptionName' type="text" name='descriptionName'/>
+
+            <label className={style.formLabel} htmlFor="category">Categoría: </label>
+            <input className={style.formInput} value={product.category} onChange={handleInputChange} id='category' type="text" name='category' />
+
+            <label className={style.formLabel} htmlFor="price">Precio: </label>
+            <input className={style.formInput} value={product.price} onChange={handleInputChange} id='price' type="number" name='price'/>
+
+            <label className={style.formLabel} htmlFor="priceBusiness">Precio venta empresas: </label>
+            <input className={style.formInput} value={product.priceBusiness} onChange={handleInputChange} id='priceBusiness' type="number" name='priceBusiness'/>
+
+            <label className={style.formLabel} htmlFor="priceVAT">Precio Consumidor Final C/IVA: </label>
+            <input className={style.formInput} value={product.priceVAT} onChange={handleInputChange} id='priceBusiness' type="number" name='priceVAT' />
+
+            <label className={style.formLabel} htmlFor="priceVATBusiness">Precio Empresa C/IVA: </label>
+            <input className={style.formInput} value={product.priceVATBusiness} onChange={handleInputChange} id='priceVATBusiness' type="number" name='priceVATBusiness' />
+
+
+        
+            <button className={style.formButton} type='submit'>Modificar producto</button>
+
+        </form>
+
+    )
+
+}
+
+
+export default UpdateProductForm;
