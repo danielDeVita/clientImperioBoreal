e35db2c18e6d2ba774091c76b6a89e4f389daac8
danielDeVita <danielitodevita@gmail.com>
2023-03-17 16:08:35 -0300

    store in maintsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import {BrowserRouter} from 'react-router-dom'
-
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux';
+import store from './Redux/store';
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 )
