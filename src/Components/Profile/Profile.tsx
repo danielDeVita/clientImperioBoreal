40ce7cddf89495d073ff2227b832f84f9b9775a6
danielDeVita <danielitodevita@gmail.com>
2023-03-21 16:15:02 -0300

    fix conflic

--- src/Components/Profile/Profile.tsx
+++ src/Components/Profile/Profile.tsx
@@ -1,21 +1,22 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { User } from "auth0";
+import React from "react";
 
 const Profile = () => {
-    const { user, isAuthenticated, isLoading } = useAuth0<User>();
-    if (isLoading) {
-        return <div>Loading ...</div>
-    }
+  const { user, isAuthenticated, isLoading } = useAuth0();
 
-    return (
-        isAuthenticated && (
-            <div>
-                <img src={user?.picture} alt={user?.name} />
-                <h2>{user?.name}</h2>
-                <p>{user?.email}</p>
-            </div>
-        )
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
+
+  return (
+    isAuthenticated && (
+      <div>
+        <img src={user?.picture} alt={user?.name} />
+        <h2>{user?.name}</h2>
+        <p>{user?.email}</p>
+      </div>
     )
+  );
 };
 
 export default Profile;
