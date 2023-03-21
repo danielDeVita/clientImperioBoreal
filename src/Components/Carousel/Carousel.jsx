024f7fcbdcc73a0d8e3aaa075e696dd7374fcd46
franco329 <franco_kuperman@hotmail.com>
2023-03-21 15:45:12 -0300

    carrousel img added, buttons carrousel color added, images sizes fixed

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -1,30 +1,59 @@
-import Carousel from 'nuka-carousel';
-import s from './Carousel.module.css';
-import BannerOne from '../../assets/banner1.jpg'
-import BannerTwo from '../../assets/banner3.jpg'
-import BannerThree from '../../assets/escolares.jpg'
+import Carousel from "nuka-carousel";
+import s from "./Carousel.module.css";
+import BannerOne from "../../assets/Banner_30oFF.png";
+import BannerTwo from "../../assets/2_X_1_AGENDAS.png";
+import BannerThree from "../../assets/banner1.jpg";
 
 const CarouselComponent = () => {
   return (
-  <Carousel className={s.CarouselContainer} autoplay={true} wrapAround = {true} autoplayInterval={4500}
-  renderCenterLeftControls={({ previousSlide }) => (
-    <button style={{marginLeft:"80px", backgroundColor:"transparent", border:"none", cursor:"pointer" }} onClick={previousSlide}>
-      <i style={{color:"black", fontSize:"50px"}} >&#60;</i>
-    </button>
-  )}
-  renderCenterRightControls={({ nextSlide }) => (
-    <button style={{marginRight:"80px", backgroundColor:"transparent", border:"none", cursor:"pointer"   }}  onClick={nextSlide}>
-      <i style={{color:"black", fontSize:"50px"}}>&#62;</i>
-    </button>
-  )}
-  renderBottomCenterControls={false}
+    <Carousel
+      className={s.CarouselContainer}
+      autoplay={true}
+      wrapAround={true}
+      autoplayInterval={4500}
+      renderCenterLeftControls={({ previousSlide }) => (
+        <button
+          className={s.iButtonLeft}
+          // style={{
+          //   marginLeft: "80px",
+          //   backgroundColor: "transparent",
+          //   border: "none",
+          //   cursor: "pointer",
+          // }}
+          onClick={previousSlide}
+        >
+          <i
+          // style={{ color: "#0fb1bd", fontSize: "50px", fontWeight: "700" }}
+          >
+            &#60;
+          </i>
+        </button>
+      )}
+      renderCenterRightControls={({ nextSlide }) => (
+        <button
+          className={s.iButtonRight}
+          // style={{
+          //   marginRight: "80px",
+          //   backgroundColor: "transparent",
+          //   border: "none",
+          //   cursor: "pointer",
+          // }}
+          onClick={nextSlide}
+        >
+          <i
+          // style={{ color: "#0fb1bd", fontSize: "50px", fontWeight: "700" }}
+          >
+            &#62;
+          </i>
+        </button>
+      )}
+      renderBottomCenterControls={false}
+    >
+      <img className={s.bannerone} src={BannerOne} alt='banner' />
+      <img className={s.bannertwo} src={BannerTwo} alt='banner' />
+      <img className={s.bannerthree} src={BannerThree} alt='banner' />
+    </Carousel>
+  );
+};
 
-  >
-    <img className={s.bannerone} src={BannerOne} alt="banner" />
-    <img className={s.bannertwo} src={BannerTwo} alt="banner" />
-    <img className={s.bannerthree} src={BannerThree} alt="banner" />
-  </Carousel>
-  )
-}
-
-export default CarouselComponent
+export default CarouselComponent;
