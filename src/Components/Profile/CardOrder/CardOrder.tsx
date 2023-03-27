cbb23a5acbb02d95d423050962a42550ea645190
danielDeVita <danielitodevita@gmail.com>
2023-03-26 22:03:03 -0300

    identation

--- src/Components/Profile/CardOrder/CardOrder.tsx
+++ src/Components/Profile/CardOrder/CardOrder.tsx
@@ -1,25 +1,24 @@
 import React from "react";
 import { UserOrder } from "../../../types";
 
-  const Card: React.FC<UserOrder> = ({user, status, cart, orderId }) => {
-    console.log(cart)
-    return (
-      <div className="card">
-        <p>Email de usuario: {user.email}</p>
-        <p>Estado: {status}</p>
-        <div className="product-list">
-          {cart.products.map(({product}: any, index) => (
-            <div key={index} className="product">
-              <img src={product?.image?.secure_url} alt={product.descriptionName} />
-              <h4>{product.descriptionName}</h4>
-              <p>Precio: {product.price}</p>
-              <p>Cantidad: {product.quantity}</p>
-            </div>
-          ))}
-        </div>
-        <p>Total: {cart.totalAmount} ARS</p>
+const Card: React.FC<UserOrder> = ({ user, status, cart, orderId }) => {
+  return (
+    <div className="card">
+      <p>Email de usuario: {user.email}</p>
+      <p>Estado: {status}</p>
+      <div className="product-list">
+        {cart.products.map((product: any, index) => (
+          <div key={index} className="product">
+            <img src={product?.image} alt={product.descriptionName} />
+            <h4>{product.descriptionName}</h4>
+            <p>Precio: {product.price}</p>
+            <p>Cantidad: {product.quantity}</p>
+          </div>
+        ))}
       </div>
-    );
-  };
-  
-  export default Card;
+      <p>Total: {cart.totalAmount} ARS</p>
+    </div>
+  );
+};
+
+export default Card;
