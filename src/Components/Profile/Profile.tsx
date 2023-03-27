42a6b661e8725fd2c48f6db6e8a4142ca67b12c2
Julianfmv <julianfmv@gmail.com>
2023-03-26 21:38:21 -0300

    userOrders in profile

--- src/Components/Profile/Profile.tsx
+++ src/Components/Profile/Profile.tsx
@@ -2,26 +2,27 @@ import { useAuth0 } from "@auth0/auth0-react";
 import styles from "./Profile.module.css"
 import { Link } from "react-router-dom";
 import Loading from "../Loading/Loading"
-import { CartContextType } from "../../types.d";
+import { CartContextType, State } from "../../types.d";
 import {useContext, useEffect} from 'react';
 import { CartContext } from "../../context";
 import { useDispatch } from "react-redux";
 import { AppDispatch } from "../../Redux/store";
 import { getUserOrders } from "../../Redux/actions";
-
-
-
-
+import CardOrder from "./CardOrder/CardOrder"
+import { useSelector } from "react-redux";
 
 
 const Profile: any = () => {
   const { user, isAuthenticated, isLoading, error, loginWithRedirect } = useAuth0();
   const { userId } = useContext(CartContext) as CartContextType
   const dispatch:AppDispatch = useDispatch();
+  
 
   useEffect(() => {
     dispatch(getUserOrders(userId))
   }, [userId])
+
+  const orders = useSelector((state: State) => state.ordersByUser)
   
   if (isLoading) {
     return <div> <Loading /></div>;
@@ -31,11 +32,11 @@ const Profile: any = () => {
     return <div>Error: {error.message}</div>;
   }
   
-
   return (
     <>
       {
         !isAuthenticated ? loginWithRedirect() : (
+          
           <div className={styles.Container}>
             <div className={styles.profileContainer}>
             <img className={styles.profileImage} src={user?.picture} alt={user?.name} />
@@ -49,9 +50,28 @@ const Profile: any = () => {
             </Link>
           </div>
             </div>
+          
+
+         
         )
       }
-    </>
+                  {
+              
+              orders.map(order => {
+                return (
+                  
+                  <CardOrder 
+                  key={order.orderId}
+                  user={order.user}
+                  status={order.status}
+                  cart={order.cart}
+                  orderId = {order.orderId}
+                  />
+                  )
+                })
+                
+              }
+      </>
   );
 };
 
