6e4fa181245ed57acd8a8c66cb35ca055bfac07d
franco329 <franco_kuperman@hotmail.com>
2023-03-27 18:28:50 -0300

    style in order

--- src/Components/Dashboard/Orders/Orders.tsx
+++ src/Components/Dashboard/Orders/Orders.tsx
@@ -3,82 +3,106 @@ import { useDispatch } from "react-redux";
 import { AppDispatch } from "../../../Redux/store";
 import { getAllOrders } from "../../../Redux/actions";
 import { useSelector } from "react-redux";
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import style from "./Orders.module.css";
+import axios from "axios";
 
 const Orders: React.FC = () => {
+  const dispatch: AppDispatch = useDispatch();
 
-    const dispatch: AppDispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getAllOrders());
+  }, []);
 
-    useEffect(() => {
-        dispatch(getAllOrders())
-    }, []);
+  const orders = useSelector((state: State) => state.orders);
 
-    const orders = useSelector((state: State) => state.orders)
+  const handleDelete = async (id: string) => {
+    try {
+      dispatch(getAllOrders());
+      const deleteProduct = await axios.delete(`/orders/${id}`);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    const handleDelete = async (id: string) => {
-        try {
-            dispatch(getAllOrders());
-            const deleteProduct = await axios.delete(`/orders/${id}`);
-        } catch (error) {
-            console.error(error)
-        }
-    };
+  const [status, setStatus] = useState("");
 
-    const [status, setStatus] = useState("")
+  const handleChange = (e: any) => {
+    setStatus(e.target.value);
+  };
 
-    const handleChange = (e: any) => {
-        setStatus(e.target.value)
-    };
+  const handleSubmit = async (
+    e: React.FormEvent<HTMLFormElement>,
+    id: string
+  ) => {
+    try {
+      e.preventDefault();
+      await axios.put(`/orders/${id}`, { status });
+      dispatch(getAllOrders());
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>, id: string) => {
-        try {
-            e.preventDefault();
-            await axios.put(`/orders/${id}`, { status });
-            dispatch(getAllOrders())
-        } catch (error) {
-            console.error(error)
-        }
-    };
-
-    return (
-        <>
-            {orders.map(order => {
+  return (
+    <>
+      <div className={style.orderContainer}>
+        {orders.map((order) => {
+          return (
+            <div className={style.orderCard}>
+              <div className={style.dataUser}>
+                <h5>Número de orden:</h5>
+                <p>{order.orderId}</p>
+                <h5>Usuario:</h5>
+                <p>{order.user.email}</p>
+                <h5>Id de usuario:</h5>
+                <p>{order.user._id}</p>
+                <h5>Estatus de compra: </h5>
+                <p>{order.status}</p>
+              </div>
+              {order.cart.products.map((product: any) => {
                 return (
-                    <div>
-                        <h2>Número de orden: {order.orderId}</h2>
-                        <h2>Usuario: {order.user.email}</h2>
-                        <h3>Id de usuario: {order.user._id}</h3>
-                        <p>Estatus de compra: {order.status}</p>
-                        <div>{order.cart.products.map((product: any) => {
-                            return (
-                                <>
-                                    <h2>{product.descriptionName}</h2>
-                                    <img src={product.image} />
-                                    <h3>Precio: {product.price}</h3>
-                                    <h3>Cantidad: {product.quantity}</h3>
-                                </>
-                            )
-                        })}
-                            <p>Total: {order.cart.totalAmount}</p>
-                        </div>
-                        <button onClick={() => { handleDelete(order.orderId) }}>Eliminar</button>
-
-                        <form onSubmit={(e) => { handleSubmit(e, order.orderId) }}>
-                            <select name="status" onChange={handleChange}>
-                                <option value="InProcess">InProcess</option>
-                                <option value="Cancelled">Cancelled</option>
-                                <option value="Paid">Paid</option>
-                            </select>
-                            <button type="submit">Modificar</button>
-                        </form>
-                        <hr />
-                        <hr />
+                  <div className={style.productCard}>
+                    <div className={style.pName}>
+                      <h5>{product.descriptionName}</h5>
+                      <img src={product.image} />
                     </div>
-                )
-            })}
-        </>
-    )
-}
+                    <div className={style.pContent}>
+                      <p>Precio: {product.price}</p>
+                      <p>Cantidad: {product.quantity}</p>
+                    </div>
+                  </div>
+                );
+              })}
+              <div className={style.pTotalAmount}>
+                <p>Total: {order.cart.totalAmount}</p>
+              </div>
 
-export default Orders
+              <form
+                onSubmit={(e) => {
+                  handleSubmit(e, order.orderId);
+                }}
+              >
+                <select name='status' onChange={handleChange}>
+                  <option value='InProcess'>InProcess</option>
+                  <option value='Cancelled'>Cancelled</option>
+                  <option value='Paid'>Paid</option>
+                </select>
+                <button type='submit'>Modificar</button>
+                <button
+                  onClick={() => {
+                    handleDelete(order.orderId);
+                  }}
+                >
+                  Eliminar
+                </button>
+              </form>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default Orders;
