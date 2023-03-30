cd895983316d25bb24bc6e419f00ff8f170970b3
danielDeVita <danielitodevita@gmail.com>
2023-03-30 13:01:38 -0300

    falta estilizar el componente y ponerle mas data

--- src/Components/PaymentStatus/PaymentStatus.jsx
+++ src/Components/PaymentStatus/PaymentStatus.jsx
@@ -9,17 +9,15 @@ const PaymentStatus = () => {
         const paymentId = searchParams.get('payment_id');
 
         async function fetchData() {
-            let response = await axios(`/payment-status?payment_id=${paymentId}`)
-            let algo = response.data
-            setStatus(algo)
-            console.log(algo)
+            let response = await axios(`mp/payment-status?payment_id=${paymentId}`)
+            setStatus(response.data.message)
         }
         fetchData()
     }, [window.location.search]);
 
     return (
         <div>
-            <h1>Payment Status</h1>
+            <h1>Estado de su compra</h1>
             <p>{status}</p>
         </div>
     );
